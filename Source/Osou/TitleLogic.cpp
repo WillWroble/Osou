95ed42cd3f5abeab4273d57d610a62f69cab4f66
WillWroble <willwroble@gmail.com>
2022-04-20 12:44:53 -0400

    Added title screen

---
+++ Source/Osou/TitleLogic.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TitleLogic.h"
+
+// Sets default values
+ATitleLogic::ATitleLogic()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATitleLogic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATitleLogic::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
